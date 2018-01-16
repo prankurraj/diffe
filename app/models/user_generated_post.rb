52c8c66dcba69f3b81cb7eaeeb03de433ac71571
Priya Gupta <priyag20061999@gmail.com>
2018-01-16 22:25:49 +0530

    Set default value for privacy

--- app/models/user_generated_post.rb
+++ app/models/user_generated_post.rb
@@ -1,6 +1,6 @@
 class UserGeneratedPost 
   include Neo4j::ActiveNode
-  property :privacy_setting, type: String
+  property :privacy_setting, type: String, default: 0 #0 for public, 1 for private
   property :text, type: String
 
 
